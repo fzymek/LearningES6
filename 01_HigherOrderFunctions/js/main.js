933bfc9c7e6b28aac9dbee96d61f3712e8fa2564
Filip <filip@MacBook-Pro-Filip.local>
2017-06-28 14:28:04 +0200

    Add examples of Higer Order Functions

---
+++ 01_HigherOrderFunctions/js/main.js
@@ -0,0 +1,34 @@
+//@flow
+
+//normal javascript function
+function multiply(a: number, b: number) {
+  return a * b;
+}
+
+//anonymous function assigned to variable
+var mul = function(a: number, b: number) {
+  return a * b;
+}
+var mul2 = mul;
+debug(mul2(5, 6));
+
+
+//animals
+var animals = [
+  {name: 'Venus', species: 'dog'},
+  {name: 'Tiger', species: 'cat'},
+  {name: 'Venus', species: 'horse'},
+  {name: 'Dżeki', species: 'dog'},
+  {name: 'Daisy', species: 'dog'},
+  {name: 'Ogonek', species: 'cat'},
+]
+
+//store filter function in variable
+var isDog = function(animal) {
+  return animal.species === 'dog'
+}
+//filter animals array for dogs and store result in variable
+var dogs = animals.filter(isDog)
+
+//print results by mapping each 'dog' object to its name using 'arrow function'
+debug("There are "+ dogs.length + " dogs: " + dogs.map((dog) => {return dog.name}).join(", ") + ".")
